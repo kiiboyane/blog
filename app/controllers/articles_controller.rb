856c344248ad1e461959f54a4ed0f6b9a745caa6
amal sahyane <amal.sahyane@gmail.com>
2019-09-06 12:48:41 +0100

    adding athentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+    http_basic_authenticate_with name: "kiibo", password: "kiibo", except: [:index, :show]
   def new
     @article = Article.new 
   end
